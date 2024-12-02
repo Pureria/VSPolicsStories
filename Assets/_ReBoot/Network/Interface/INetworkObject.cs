3790fdd2d21a1bb10c8d44f2b6cf811c90bf6df0
Pureria <117608289+Pureria@users.noreply.github.com>
2024-12-02 23:38:13 +0900

    インターフェースにAwakeを追加

--- Assets/_ReBoot/Network/Interface/INetworkObject.cs
+++ Assets/_ReBoot/Network/Interface/INetworkObject.cs
@@ -6,14 +6,16 @@ namespace _ReBoot.Network
 {
     public interface INetworkObject
     {
-        public void OnNetworkStart();
-        public void OnNetworkPreUpdate();
-        public void OnNetworkFixedUpdate();
-        public void OnNetworkLateUpdate();
+        public abstract void OnNetworkAwake();
+        public abstract void OnNetworkStart();
+        public abstract void OnNetworkPreUpdate();
+        public abstract void OnNetworkFixedUpdate();
+        public abstract void OnNetworkLateUpdate();
     }
 
     public interface INetworkHostFunction: INetworkObject
     {
+        public abstract void OnHostAwake();
         public abstract void OnHostStart();
         public abstract void OnHostPreUpdate();
         public abstract void OnHostFixedUpdate();
@@ -22,6 +24,7 @@ namespace _ReBoot.Network
 
     public interface INetworkClientFunction : INetworkObject
     {
+        public abstract void OnClientAwake();
         public abstract void OnClientStart();
         public abstract void OnClientPreUpdate();
         public abstract void OnClientFixedUpdate();
@@ -30,6 +33,7 @@ namespace _ReBoot.Network
     
     public interface INetworkOwnerFunction : INetworkObject
     {
+        public abstract void OnOwnerAwake();
         public abstract void OnOwnerStart();
         public abstract void OnOwnerPreUpdate();
         public abstract void OnOwnerFixedUpdate();
--- Assets/_ReBoot/Network/Interface/NetworkObjectBase.cs
+++ Assets/_ReBoot/Network/Interface/NetworkObjectBase.cs
@@ -7,21 +7,25 @@ namespace _ReBoot.Network
 {
     public partial class NetworkObjectBase : NetworkBehaviour , INetworkHostFunction, INetworkOwnerFunction, INetworkClientFunction
     {
+        public virtual void OnNetworkAwake() { }
         public virtual void OnNetworkStart() { }
         public virtual void OnNetworkPreUpdate() { }
         public void OnNetworkFixedUpdate() { }
         public void OnNetworkLateUpdate() { }
         
+        public void OnHostAwake() { }
         public void OnHostStart() { }
         public void OnHostPreUpdate() { }
         public void OnHostFixedUpdate() { }
         public void OnHostLateUpdate() { }
 
+        public void OnOwnerAwake() { }
         public void OnOwnerStart() { }
         public void OnOwnerPreUpdate() { }
         public void OnOwnerFixedUpdate() { }
         public void OnOwnerLateUpdate() { }
 
+        public void OnClientAwake() { }
         public void OnClientStart() { }
         public void OnClientPreUpdate() { }
         public void OnClientFixedUpdate() { }
